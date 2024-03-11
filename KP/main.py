0e0323cbe90717da9a26678801ad917808bd2fea
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2024-03-11 18:45:42 +0300

    a add

--- KP/main.py
+++ KP/main.py
@@ -3,7 +3,7 @@ import datetime
 from pprint import pprint
 from config import open_weather_token
 
-def get_wether(city, open_weather_token):
+def get_weather(city, open_weather_token):
     try:
         r = requests.get(
             f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric")
@@ -30,7 +30,7 @@ def get_wether(city, open_weather_token):
 
 def main():
     city = input("Введите город: ")
-    get_wether(city, open_weather_token)
+    get_weather(city, open_weather_token)
 
 if __name__ == '__main__':
     main()
