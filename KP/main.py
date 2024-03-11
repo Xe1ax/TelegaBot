796d91fb313514d568a66f8bcf01772202e7a54c
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2024-03-11 18:20:15 +0300

    output correction
    output format update

--- KP/main.py
+++ KP/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 from pprint import pprint
 from config import open_weather_token
 
@@ -7,7 +8,21 @@ def get_wether(city, open_weather_token):
         r = requests.get(
             f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric")
         data = r.json()
-        pprint(data)
+        #pprint(data)
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        general_forecast = data["weather"]["description"]
+
+        print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C°\nОщущается как: {feels_like}C°\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/c\n"
+              f"Общий прогноз: {general_forecast}"
+              )
 
     except Exeption as ex:
         print(ex)
