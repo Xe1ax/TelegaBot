569cac90c7d7eabfe5cf6f7d61e31d521e037c06
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2024-03-11 17:17:51 +0300

    quick look
    Weather bot start

---
+++ KP/config.py
@@ -0,0 +1,1 @@
+open_weather_token = 'e23fb0e2e0b629892d87358e5c515d63'
---
+++ KP/main.py
@@ -0,0 +1,20 @@
+import requests
+from config import open_weather_token
+
+def get_wether(city, open_weather_token):
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}")
+        data = r.json()
+        print(data)
+
+    except Exeption as ex:
+        print(ex)
+        print("Проверьте название города")
+
+def main():
+    city = input("Введите город: ")
+    get_wether(city, open_weather_token)
+
+if __name__ == '__main__':
+    main()
